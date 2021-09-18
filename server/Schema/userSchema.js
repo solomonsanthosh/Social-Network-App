0f12f9010daadc22b9c42f7cb2f92f174b715803
solomonsanthosh <solosanty2064@gmail.com>
2021-09-18 05:47:14 -0700

    first commit

---
+++ server/Schema/userSchema.js
@@ -0,0 +1,53 @@
+const { createHmac } = await import('crypto');
+import { v4 as uuidv4, v4 } from 'uuid';
+const mongoose  = require('mongoose');
+
+const userSchema = new mongoose.Schema({   
+   name: {
+      type: String,
+      required: true,
+      trim: true
+      
+   },
+   email: {
+      type: String,
+      required: true,
+      trim: true
+      
+   },
+   hashed_password: {
+      type: String,
+      required: true
+   },
+   salt: String,
+   created: {
+      type: Date,
+      default: Date.now
+   },
+   updated: Date
+   
+}
+)
+userSchema.methods = {
+   encrptPassword: function(password) {
+      try {
+         return createHmac('sha1', this.salt)
+         .update(password)
+         .digest('hex');
+   
+      }catch (err) {
+         return '';
+      }
+   }
+}
+
+userSchema.virtual('password').set((password)=>{
+   this._password = password
+   this.salt = uuidv4()
+   this.hashed_password = this.encrptPassword(password)
+
+}).get(()=>{
+   return this._password
+})
+
+module.exports = mongoose.model('User',userSchema)
